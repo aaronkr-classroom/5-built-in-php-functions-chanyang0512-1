78543e24b3a0590a6b6145e117fe6a6a802b6e03
chanyang0512 <cksdid030512@gmail.com>
2026-04-07 16:52:02 +0900

    Add files via upload

--- 0_var_dump.php
+++ 0_var_dump.php
@@ -1,10 +1,26 @@
 <?php
-// @TODO
+$username = 'Ivy'; // 사칼라 함수
+$user_array1 = ['Ivy', 34, 'Web programming']; //배열
+$user_array2 = [//키/값/배열
+      'name' => 'Ivy',
+      'age' => 34,
+      'majar' => 'Web programming',
+];
+
+class User {
+  public $name;
+  public $age;
+  public $majar;
+  public function __construct($name, $age, $majar) {
+    // => 정위 할 때 매개 변수
+    $this -> name = $name;
+    $this -> age = $age;
+    $this -> majar = $majar;
+  } 
+}
+$user_object = new User('Ivy', 34, 'Web programming')
 ?>
-<?php include 'includes/header.php' ?>
-
   <p>Scalar: <?php var_dump($username); ?></p>
-  <p>Array:  <?php var_dump($user_array); ?></p>
+  <p>Array1:  <?php var_dump($user_array1); ?></p>
+  <p>Array2:  <?php var_dump($user_array2); ?></p>
   <p>Object: <?php var_dump($user_object); ?></p>
-
-<?php include 'includes/footer.php' ?>
--- 2_finding-characters.php
+++ 2_finding-characters.php
@@ -1,21 +1,26 @@
 <?php
 $text = 'Home sweet home';
 ?>
-<?php include 'includes/header.php'; ?>
+
 <p>
-  <b>First match (case-sensitive):</b>
-  <br>
-  <b>First match (not case-sensitive):</b>
-  <br>
-  <b>Last match (case-sensitive):</b>
-  <br>
-  <b>Last match (not case-sensitive):</b>
-  <br>
-  <b>Text after first match (case-sensitive):</b>
-  <br>
-  <b>Text after first match (not case-sensitive):</b>
-  <br>
-  <b>Text between two positions:</b>
-  <br>
+  <b>First match (case-sensitive): </b>
+  <?= strpos($text, 'ho') ?>(11)<br>
+ 
+  <b>First match (not case-sensitive): </b>
+  <?= stripos($text, 'me', 5) ?>(13)<br>
+ 
+  <b>Last match (case-sensitive): </b>
+  <?= strrpos($text,'ho') ?> (0)<br>
+ 
+  <b>Last match (not case-sensitive): </b>
+  <?= strripos($text, 'Ho') ?> (11)<br>
+ 
+  <b>Text after first match (case-sensitive): </b>
+  <?= strstr($text, 'ho') ?> (home)<br>
+ 
+  <b>Text after first match (not case-sensitive): </b>
+  <?= stristr($text, 'ho') ?>(Home sweet home)<br>
+ 
+  <b>Text between from position 1 to length: </b>
+  <?= substr($text, 5, 5) ?> (sweet)<br>
 </p>
-<?php include 'includes/footer.php'; ?>
